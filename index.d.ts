45183b95ecba31b08d6ec19b7ab5a11f918f0eb5
liuxieyong <xyliu@dcjet.com.cn>
2020-03-30 18:24:51 +0800

    some

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 
-// /**类声明*/
+/**类声明*/
 declare class JVD{
 
      isString() : JVD
@@ -7,3 +7,51 @@ declare class JVD{
 }
 
 declare function JVD(s:string): JVD
+
+export = JVD
+
+// declare namespace JVD {
+// 	interface Options {
+// 		/**
+// 		Set to `false` to avoid spawning subprocesses and instead only resolve the locale from environment variables.
+// 		@default true
+// 		*/
+// 		readonly spawn?: boolean;
+// 	}
+// }
+
+// declare const JVD: {
+// 	/**
+// 	@returns The locale.
+// 	@example
+// 	```
+// 	const locale = require('locale.js');
+// 	locale.set(['demo','real'])
+	
+// 	console(locale('demo'))
+// 	console(locale.get('demo'))
+// 	```
+// 	*/
+// 	(str:string,lang?: string): JVD;
+
+
+// 	/**
+// 	 * get locale string
+// 	 * 获取本地化（国际化）字符
+// 	 * @param str 原始字符
+// 	 * @param lang 语言
+// 	 @returns The locale string.
+// 	 */
+// 	isString(str:string,lang?: string): JVD;
+
+// 	/**
+// 	 * add locale map to cache 
+// 	 * 添加国际化支持
+// 	 * @param arrayOrJson 数组或者json
+// 	 * @param lang  语言
+// 	 */
+// 	isNumber(arrayOrJson , lang?: string) :JVD ;
+
+// };
+
+// export = JVD;
--- test.js
+++ test.js
@@ -10,6 +10,16 @@ it('test reg',()=>{
             return true
         return false
     })
-    jvd.isTest('hello')
-    jvd2.isTest('hi')
+    expect(typeof jvd.isTest).toBe('function')
+    expect(jvd2.isTest).toBe(undefined)
 })
+
+
+var a = require('./defautImpl')
+
+console.log(a)
+
+var b= {
+    aa : JVD().requiered().isNumber().gt(100).lt(200),
+    "bb" : JVD().isString().match(/aabb/g)
+}
